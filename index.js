1d786550877b636ca9fce615af899bd47b612ab1
Maksim Yudzeshka <yudzeshka@gmail.com>
2021-10-19 16:47:51 +0300

    add randomDate and randomNumber functions

--- index.js
+++ index.js
@@ -14,4 +14,56 @@ function randomDate(start, end) {
   );
 }
 
-console.log(randomDate(new Date(2012, 0, 1), new Date()));
+console.log(randomDate(new Date(2012, 0, 1), new Date()).toJSON().split("T")[0]);
+
+
+
+import fetch from 'node-fetch';
+
+const apiBase = 'http://localhost:3000';
+
+const Endpoints = {
+  users: '/users',
+};
+
+const getUsers = async () => {
+  const response = await fetch(`${apiBase}${Endpoints.users}`);
+  const users = await response.json();
+
+  return users;
+};
+
+const addUser = async (user) => {
+  const newUserResponse = await fetch(`${apiBase}${Endpoints.users}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    body: JSON.stringify(user),
+  });
+
+  const result = await newUserResponse.json();
+  return result;
+};
+const fnArr = ['Andrea', 'Emilie', 'Sharon', 'Bujar', 'Florival', 'Laure', 'Olivia', 'Zaide', 'Felix', 'Oswin'];
+const lnArr = ['Sørensen', 'Kanestrøm', 'Hart', 'Arnaud', 'Monteiro', 'Giraud', 'Omahony', 'Moraes', 'Singh', 'Schurig']
+const main = async () => {
+  console.log(await getUsers());
+
+  const user = {
+
+    firstName: 'John',
+    lastName: 'Doe',
+    birthDate: '1995-05-23', // between 13 - 18
+  };
+
+  console.log(await addUser(user));
+};
+
+main().then();
+
+
+
+function randomNumber(arr) {
+   return arr[Math.floor(Math.random() * arr.length)];
+}
+
+console.log(randomNumber(fnArr));
